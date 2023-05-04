3c7a0e9656dfdf32e8b672ef083cc991bae0b398
Sergio Rojas <sergio.rojas@bdp.com.bo>
2023-05-04 16:35:48 -0400

    con firebase

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DataLocalService } from '../services/data-local.service';
 import { Registro } from '../models/registro.model';
+
+import { Firestore, collection, collectionData} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface SucursalesProfile {
+  nombre: string,
+  direccion: string,
+  horarios: string,
+  ubicacion: string
+};
+interface ServiciosProfile {
+  otro: string,
+  cortes: {
+    detalle: string
+    foto: string,
+    titulo: string
+  }
+}
 
 @Component({
   selector: 'app-tab3',
@@ -8,16 +26,35 @@ import { Registro } from '../models/registro.model';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  private firestore: Firestore = inject(Firestore);
 
-  constructor( public dataLocal: DataLocalService ) {}
+  sucursales$: Observable<SucursalesProfile[]>;
+  servicios$: Observable<ServiciosProfile[]>;
+
+  constructor( public dataLocal: DataLocalService ) {
+    // get a reference to the user-profile collection
+    const sucursalesProfileCollection = collection(this.firestore, 'sucursales');
+    const serviciosProfileCollection = collection(this.firestore, 'servicios');
+
+    // get documents (data) from the collection using collectionData
+    this.sucursales$ = collectionData(sucursalesProfileCollection) as Observable<SucursalesProfile[]>;
+    this.servicios$ = collectionData(serviciosProfileCollection) as Observable<ServiciosProfile[]>;
+    console.log(this.sucursales$);
+    console.log(this.servicios$);
+
+    const servicios = collection(this.firestore, 'servicios');
+    collectionData(serviciosProfileCollection).subscribe((r)=> {
+      console.log(r);
+    });
+  }
 
   abrirRegistro( registro: Registro  ) {
-    console.log('Registro', registro );
+    //console.log('Registro', registro );
     this.dataLocal.abrirRegistro( registro );
   }
   abrirEnlace(geo: string) {
-    console.log('Registro', geo );
     this.dataLocal.abrirEnlace(geo)
   }
 
 }
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA9WYicMqSFevrXZZj24t4V-9swpky8_ts",
+    authDomain: "polaris-edb14.firebaseapp.com",
+    databaseURL: "https://polaris-edb14.firebaseio.com",
+    projectId: "polaris-edb14",
+    storageBucket: "polaris-edb14.appspot.com",
+    messagingSenderId: "165383787260",
+    appId: "1:165383787260:web:89e79c861fc11dc52a7d79",
+    measurementId: "G-RB5ZV5W5VG"
+  }
 };
