354ba75c0c07218e8dc726588324d0d33e7be2e7
Sergio Rojas <sergio.rojas@bdp.com.bo>
2023-05-05 17:11:58 -0400

    Ya incluye imagenes para los servicios

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CollectionReference, Firestore, collection, collectionData, getDoc, doc, getDocs} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface ServiciosProfile {
+  foto: string,
+  titulo: string,
+  grupo: string,
+  cortes: {
+    detalle: string
+    foto: string,
+    titulo: string
+  }
+}
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +20,16 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  private firestore: Firestore = inject(Firestore);
+  servicios$: Observable<ServiciosProfile[]>;
+
+  constructor() {
+    const serviciosProfileCollection = collection(this.firestore, 'servicios');
+    this.servicios$ = collectionData(serviciosProfileCollection) as Observable<ServiciosProfile[]>;
+    const servicios2 = collection(this.firestore, '/servicios/166XyboxLuhDWoHrcRtE/cortes/4MGQq8F3RnWEIy4rIeen/fotos');
+    collectionData(servicios2).subscribe((r)=> {
+      console.log(r);
+    });
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,23 +2,14 @@ import { Component, inject } from '@angular/core';
 import { DataLocalService } from '../services/data-local.service';
 import { Registro } from '../models/registro.model';
 
-import { Firestore, collection, collectionData} from '@angular/fire/firestore';
+import { CollectionReference, Firestore, collection, collectionData, getDoc, doc, getDocs} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
 interface SucursalesProfile {
   nombre: string,
   direccion: string,
   horarios: string,
   ubicacion: string
 };
-interface ServiciosProfile {
-  otro: string,
-  cortes: {
-    detalle: string
-    foto: string,
-    titulo: string
-  }
-}
 
 @Component({
   selector: 'app-tab3',
@@ -29,23 +20,14 @@ export class Tab3Page {
   private firestore: Firestore = inject(Firestore);
 
   sucursales$: Observable<SucursalesProfile[]>;
-  servicios$: Observable<ServiciosProfile[]>;
 
   constructor( public dataLocal: DataLocalService ) {
     // get a reference to the user-profile collection
     const sucursalesProfileCollection = collection(this.firestore, 'sucursales');
-    const serviciosProfileCollection = collection(this.firestore, 'servicios');
 
     // get documents (data) from the collection using collectionData
     this.sucursales$ = collectionData(sucursalesProfileCollection) as Observable<SucursalesProfile[]>;
-    this.servicios$ = collectionData(serviciosProfileCollection) as Observable<ServiciosProfile[]>;
-    console.log(this.sucursales$);
-    console.log(this.servicios$);
-
-    const servicios = collection(this.firestore, 'servicios');
-    collectionData(serviciosProfileCollection).subscribe((r)=> {
-      console.log(r);
-    });
+    
   }
 
   abrirRegistro( registro: Registro  ) {
